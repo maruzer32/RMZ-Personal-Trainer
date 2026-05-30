15d7fa07537a1a9b8efb43a3b7283b6209644ac1
RUBEN MARIO ZERDAN <120148627+maruzer32@users.noreply.github.com>
2026-05-30 02:28:10 -0300

    Add files via upload

---
+++ personal_trainer.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const navLinks = document.querySelectorAll('.navbar-nav .nav-link');
+    const navbarCollapse = document.querySelector('.navbar-collapse');
+
+    navLinks.forEach(function (link) {
+        link.addEventListener('click', function () {
+            if (navbarCollapse.classList.contains('show')) {
+                const bsCollapse = new bootstrap.Collapse(navbarCollapse, {
+                    toggle: false
+                });
+                bsCollapse.hide();
+            }
+        });
+    });
+});
